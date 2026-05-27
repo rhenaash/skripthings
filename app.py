7f3b08d5ed2aebd163da1dc771a3d4d0a6dbf571
rhenaash <rheformcy@gmail.com>
2026-05-28 01:13:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -277,7 +277,7 @@ if uploaded_file is not None:
 
                         try:
                             tf.random.set_seed(49)
-                            random.seed(49)
+                            np.random.seed(49)
                             clear_session()
 
                             model = Sequential([
@@ -300,6 +300,7 @@ if uploaded_file is not None:
                                 y_tr,
                                 epochs=10,
                                 batch_size=batch,
+                                shuffle=False,
                                 verbose=0
                             )
 
