2ba319d417866aeab54ab3265ca1932ccc0298d3
rhenaash <rheformcy@gmail.com>
2026-05-28 00:44:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -276,7 +276,7 @@ if uploaded_file is not None:
                         dropout = float(p[3])
 
                         try:
-                            tf.random.set_seed(49),
+                            tf.random.set_seed(49)
                             np.random.set(49)
                             clear_session()
 
