b2c3dbad5a10a8f9e0d5d5596601e6b93fd15332
rhenaash <rheformcy@gmail.com>
2026-05-28 06:43:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import tensorflow as tf
 # PENGUNCIAN SEED UNTUK REPRODUKSIBILITAS
 # ==========================================
 SEED_VALUE = 49
-random.seed(123)
+random.seed()
 np.random.seed(SEED_VALUE)
 tf.random.set_seed(SEED_VALUE)
 
