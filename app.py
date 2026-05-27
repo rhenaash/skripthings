e110363c05c16fda3ebede3cff1d1281ce729ee0
rhenaash <rheformcy@gmail.com>
2026-05-28 06:33:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -360,7 +360,9 @@ if uploaded_file is not None:
             # =====================================================
             # LOOP PSO
             # =====================================================
-            np.random.seed(123)
+            np.random.seed(49)
+            tf.random.set_seed(49)
+
             for it in range(PSOSL_iters):
 
                 costs_PSOSL = pso_obj_PSOSL(optimizer.swarm.position)
@@ -464,7 +466,7 @@ if uploaded_file is not None:
             # =====================================================
             # FINAL TRAINING
             # =====================================================
-            random.seed(123)
+            np.random.seed(49)
             GRU_PSOSL = Sequential([
                 Input(shape=(X_train.shape[1], X_train.shape[2])),
             
