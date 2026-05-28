ce0ed1100265b686bc73d67d7d85b91a9ccb2fef
rhenaash <rheformcy@gmail.com>
2026-05-28 08:33:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -276,8 +276,7 @@ if uploaded_file is not None:
                         batch = int(np.round(p[2]))
                         dropout = float(p[3])
                         try:
-                            random.seed(193)
-                            tf.random.set_seed(49)
+                            random.seed(189)
                             clear_session()
                         
                             model = Sequential([
