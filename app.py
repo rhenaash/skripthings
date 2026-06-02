6321fbcffb40ad97fe496a5c5e73cdfd60875e10
rhenaash <rheformcy@gmail.com>
2026-06-03 05:22:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -416,7 +416,7 @@ if uploaded_file is not None:
     
                                 try:
                                     SEED = 49
-                                    tf.random.set_seed(181)
+                                    tf.random.set_seed(188)
                                     random.seed(49)
                                     clear_session()
     
