bc52f891de85a515c1292295ee37a93af8d3db57
rhenaash <rheformcy@gmail.com>
2026-06-02 15:23:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -356,10 +356,11 @@ if uploaded_file is not None:
     
             st.write(f"Jumlah Outlier ditemukan: {len(outliers)}")
             df_display = outliers.copy()
-            if 'Tanggal' in df_display.columns:
-                df_display['Tanggal'] = df_display['Tanggal'].dt.strftime('%Y-%m-%d')
-            st.dataframe(df_display, hide_index=True)
-            st.dataframe(outliers)
+            df_display['Tanggal'] = pd.to_datetime(
+                df_display['Tanggal']
+            ).dt.strftime('%Y-%m-%d')
+            
+            st.dataframe(df_display)
 
     # ===========================================================
     # TAB 1 — GRU-PSO
