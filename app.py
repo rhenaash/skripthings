a077aa3c80a777f92e5df1b8d6a0a4b2858441b3
rhenaash <rheformcy@gmail.com>
2026-06-12 06:01:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -495,7 +495,7 @@ if uploaded_file is not None:
                     # =====================================================
                     # LOOP PSO
                     # =====================================================
-                    tf.random.set_seed(181)
+                    tf.random.set_seed(49)
                     max_iter_colab = df_colab_cost['iteration'].max()
     
                     for it in range(PSOSL_iters):
